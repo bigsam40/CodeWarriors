caf85ac83b856f4105826a799a76a243a691f664
bigsam40 <117366036+bigsam40@users.noreply.github.com>
2023-05-04 22:47:35 +0400

    new Exercise();

---
+++ BinaryAddition/BinaryAddition.java
@@ -0,0 +1,12 @@
+package CodeWarriors.BinaryAddition;
+
+public class BinaryAddition {
+    // for test
+    public static void main(String[] args) {
+
+    }
+
+    public static String binaryAddition(int a, int b){
+        return Integer.toBinaryString(a+b);
+    }
+}
