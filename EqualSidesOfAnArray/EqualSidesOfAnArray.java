a0e95a3258f400aaefb451d9ed5be73712858663
bigsam40 <117366036+bigsam40@users.noreply.github.com>
2023-05-04 23:19:31 +0400

    new Exercise();

---
+++ EqualSidesOfAnArray/EqualSidesOfAnArray.java
@@ -0,0 +1,26 @@
+package CodeWarriors.EqualSidesOfAnArray;
+
+import java.util.Arrays;
+
+public class EqualSidesOfAnArray {
+    // for test
+    public static void main(String[] args) {
+
+    }
+
+    public static int findEvenIndex(int[] arr) {
+        if (arr.length < 1) return -1;
+        for (int i = 0; i < arr.length; i++) {
+            int leftSum = 0;
+            for (int j = i-1; j >= 0; j--) {
+                leftSum += arr[j];
+            }
+            int rightSum = 0;
+            for (int j = i+1; j < arr.length; j++) {
+                rightSum += arr[j];
+            }
+            if (leftSum == rightSum) return i;
+        }
+        return -1;
+    }
+}
