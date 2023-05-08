7ea9c9c6e2a1c7be2a6e28ed2f4a930692332f55
bigsam40 <117366036+bigsam40@users.noreply.github.com>
2023-05-08 14:52:40 +0400

    new Exercise();

---
+++ CountIPAddresses/CountIPAddresses.java
@@ -0,0 +1,28 @@
+package CodeWarriors.CountIPAddresses;
+
+public class CountIPAddresses {
+    // for tests
+    public static void main(String[] args) {
+        System.out.println(ipsBetween("20.0.0.10", "20.0.1.0"));
+        System.out.println(ipsBetween("10.0.0.0", "10.0.0.50"));
+        System.out.println(ipsBetween("0.0.0.0", "255.255.255.255"));
+    }
+
+    // target method
+    public static long ipsBetween(String start, String end) {
+        return Math.abs(getNumericIpView(start) - getNumericIpView(end));
+    }
+
+    public static long getNumericIpView(String ipAddress) {
+        long numericIpView = 0;
+        try {
+            String[] octets = ipAddress.split("\\.");
+            for (int i = 0; i < 4; i++) {
+                numericIpView += (Math.pow(256, 3 - i) * Integer.parseInt(octets[i]));
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+        return numericIpView;
+    }
+}
